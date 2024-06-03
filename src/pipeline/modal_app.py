344b1814dec4684b8d45d97e8904b7409c269afa
oleg <oleg.roshka@proton.me>
2024-06-03 17:59:12 +0100

    modal

--- src/pipeline/modal_app.py
+++ src/pipeline/modal_app.py
@@ -2,6 +2,8 @@ import modal
 
 from src.pipeline.etf_pipeline import run_pipeline
 
+image = modal.Image.debian_slim().pip_install_from_requirements("/mnt/data/requirements.txt")
+
 app = modal.App(
     "FolioLLM-pipeline"
 )  # Note: prior to April 2024, "app" was called "stub"
