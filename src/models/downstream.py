0e2060b49b9becf2687a513e8c18f78a86bce53a
nov3o <ilya.badaneen@gmail.com>
2024-06-03 22:46:37 +0300

    random selector

--- src/models/downstream.py
+++ src/models/downstream.py
@@ -8,16 +8,38 @@ from transformers import AutoModel, AutoTokenizer, AutoModelForCausalLM
 
 
 etfs_num = 11796
-# etfs_num = 5
 vocab_size = 151936
 
 
+def select_scores(
+        heatmap, do_sample=False, top_p=0.2, top_k=50, temperature=1,
+        sorted_return=True, standardize=False
+):
+    if standardize: 
+        heatmap -= heatmap.mean()
+        heatmap /= heatmap.std()
+
+    p = torch.nn.functional.softmax(heatmap, 0)
+    val, ind = p.sort(descending=True)
+    val, ind = val[:top_k], ind[:top_k]
+    m = int((val.cumsum(0) < val.sum()*top_p).sum(dim=-1))
+
+    if not do_sample: return ind[:m], torch.arange(m)
+
+    T = 10/(temperature+1)
+    ord_stat = torch.multinomial(val**T, m, replacement=False)
+
+    sorted_indices = ord_stat.sort()[0]
+    return ind[sorted_indices], sorted_indices 
+
+# temperature increases randomness of sampling (0,+infty)
 sel_cfg = {
-    'do_sample': True,  # 
-    'temperature': 0.7,  #
-    'top_p': 0.9,
+    'do_sample': False,  # 
+    'temperature': 0,  #
+    'top_p': 0.2,
     'top_k': 50,
 }
+
 
 class DownstreamLM(nn.Module):
     def __init__(self, body_path, class_path=None, select_path=None, sel_cfg=sel_cfg):
@@ -25,8 +47,7 @@ class DownstreamLM(nn.Module):
         self.model = AutoModelForCausalLM.from_pretrained(
             body_path, output_hidden_states=True
         )
-        # self.model = nn.Embedding(vocab_size, 1024)
-        
+       
         self.class_head = nn.Linear(1024, 2, bias=False)
         # self.select_head = ...  # something more complicated
         self.select_head = nn.Linear(1024, etfs_num, bias=False)
@@ -52,6 +73,10 @@ class DownstreamLM(nn.Module):
         return self.class_head(out_token)
 
 
+    def _heatmap_selection(scores):
+        d = 1024
+        heatmap / torch.sqrt(d)
+
     def select(self):
         # X-Attn of out tokens (Batch, Seq, Hid)
         return self.select_head(self.out[:, -1])
@@ -59,11 +84,11 @@ class DownstreamLM(nn.Module):
 
 model_name = "FINGU-AI/FinguAI-Chat-v1" # output_dir
 
-m = DownstreamLM(model_name)
-text = "hello yann lecun"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-tokens = tokenizer(text, return_tensors='pt')
+# m = DownstreamLM(model_name)
+# text = "hello yann lecun"
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# tokens = tokenizer(text, return_tensors='pt')
 
-# verification everything works
-m.classify(**tokens)
-_ = m.select()
+# # verification everything works
+# m.classify(**tokens)
+# _ = m.select()
