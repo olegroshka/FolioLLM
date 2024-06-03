c743e1c7175b33bdfaba783c721dafa539c374f5
oleg <oleg.roshka@proton.me>
2024-06-03 18:56:37 +0100

    modal

--- src/pipeline/modal_app.py
+++ src/pipeline/modal_app.py
@@ -7,7 +7,7 @@ from src.pipeline.etf_pipeline import run_pipeline
 # Create an image with the necessary dependencies from requirements.txt
 image = modal.Image.debian_slim().pip_install_from_requirements("requirements.txt")
 
-# Define the application with mounts
+# Define the application with mounts and GPU requirements
 app = modal.App(
     "FolioLLM-pipeline",
     image=image,
@@ -18,24 +18,16 @@ app = modal.App(
 )
 
 
-@app.function()
+@app.function(gpu="A100")  # Request a specific GPU type, e.g., A100, V100, etc.
 def run():
     # Define the absolute path for the JSON file
-    etf_data_palin_file = "/root/data/etf_data_v3_plain.json"
-    test_prompts_file = "/root/data/basic-competency-test-prompts-1.json"
-    training_prompts_template_file = "/root/data/training-template-adv.json"
-    etf_data_cleaned_file = "/root/data/etf_data_v3_clean.json"
+    json_structured_file = "/root/data/etf_data_v3_plain.json"
 
     # Ensure the W&B API key is set from the secret
     wandb_api_key = os.environ.get("WANDB_API_KEY")
     os.environ["WANDB_API_KEY"] = wandb_api_key
 
-    run_pipeline(
-        model_name='FINGU-AI/FinguAI-Chat-v1',
-        json_structured_file=etf_data_palin_file,
-        test_prompts_file=test_prompts_file,
-        json_prompt_response_template_file=training_prompts_template_file,
-        json_prompt_response_file_cleaned=etf_data_palin_file)
+    run_pipeline(json_structured_file=json_structured_file)
 
 
 @app.local_entrypoint()
