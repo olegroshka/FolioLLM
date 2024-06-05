fa216fb9ca4de3d9baa7958a57e9915ce8ce14d4
nov3o <ilya.badaneen@gmail.com>
2024-06-05 15:28:50 +0300

    other models for classify comarison added

--- src/training/test_classification_comparison.py
+++ src/training/test_classification_comparison.py
@@ -6,8 +6,19 @@ embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 
 df = pd.read_csv('../../data/classifier.csv')
 
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from peft import PeftModel
+
 model_name = 'FINGU-AI/FinguAI-Chat-v1'
 m = MultitaskLM(model_name)
+
+MODEL_TYPE = "base" # \in ['base', 'lora_low', 'lora_high']
+
+m.model = {
+    'lora_low': PeftModel.from_pretrained(m.model, '../pipeline/lora_low/FINGU-AI/FinguAI-Chat-v1'),
+    'lora_high': PeftModel.from_pretrained(m.model, '../pipeline/lora_high/FINGU-AI/FinguAI-Chat-v1'),
+    'base': m.model,
+}[MODEL_TYPE]
 
 texts_label_0 = df[df['label'] == 0]['text'].tolist()
 texts_label_1 = df[df['label'] == 1]['text'].tolist()
@@ -23,6 +34,9 @@ so = embedding_model.encode(texts_label_0)
 sg = embedding_model.encode(texts_label_1)
 
 fom, fgm, som, sgm = fo.mean(0), fg.mean(0), so.mean(0), sg.mean(0)
-folio_acc = (((fo @ fom) > (fo @ fgm)).sum() + ((fg @ fgm) > (fg @ fom)).sum()) / 281 # 98%
-sentr_acc = (((so @ som) > (so @ sgm)).sum() + ((sg @ sgm) > (sg @ som)).sum()) / 281 # 87%
+folio_acc = (((fo @ fom) > (fo @ fgm)).sum() + ((fg @ fgm) > (fg @ fom)).sum()) / 281 # fingu_base=98.22%, fingu_low_lora=98.93%, fingu_high_lora=99.29%
+# sentr_acc = (((so @ som) > (so @ sgm)).sum() + ((sg @ sgm) > (sg @ som)).sum()) / 281 # 87%
+sentr_acc = -1
 print(folio_acc, sentr_acc)
+
+
