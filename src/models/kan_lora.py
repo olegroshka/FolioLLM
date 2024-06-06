2a3b0afb5d67a3912757676aaf8283234c585a71
oleg <oleg.roshka@proton.me>
2024-06-07 00:03:42 +0100

    kan lora experiments

--- src/models/kan_lora.py
+++ src/models/kan_lora.py
@@ -1,3 +1,5 @@
+import math
+
 from peft.tuners.lora import LoraLayer
 from torch import nn
 from src.models.kan import KAN
@@ -28,15 +30,55 @@ class KANLayer(nn.Module):
         return self.final_linear.bias
 
 def patch_update_kan_lora_layer():
-    original_update_layer = LoraLayer.update_layer
 
-    def new_update_lora_layer(self, adapter_name, r, lora_alpha, lora_dropout, init_lora_weights, use_rslora, use_dora: bool = False, **kwargs):
-        if isinstance(self, LoraLayer):
-            self.lora_A[adapter_name] = KANLayer(self.in_features, r)
-            self.lora_B[adapter_name] = KANLayer(r, self.out_features)
-            self.lora_dropout[adapter_name] = nn.Dropout(p=lora_dropout)  # Ensure the adapter_name exists in lora_dropout
-            return self
-        return original_update_layer(self, adapter_name, r, lora_alpha, lora_dropout, init_lora_weights, use_rslora, use_dora, **kwargs)
+    def new_update_lora_layer(self, adapter_name, r, lora_alpha, lora_dropout, init_lora_weights, use_rslora,
+                              use_dora: bool = False, **kwargs):
+        # Call the original method to handle the initialization
+        # This code works for linear layers, override for other layer types
+        if r <= 0:
+            raise ValueError(f"`r` should be a positive integer value but the value passed is {r}")
+
+        self.r[adapter_name] = r
+        self.lora_alpha[adapter_name] = lora_alpha
+        if lora_dropout > 0.0:
+            lora_dropout_layer = nn.Dropout(p=lora_dropout)
+        else:
+            lora_dropout_layer = nn.Identity()
+
+        self.lora_dropout.update(nn.ModuleDict({adapter_name: lora_dropout_layer}))
+
+        # Now replace(!) lora_A and lora_B with KANLayer
+        self.lora_A[adapter_name] = KANLayer(self.in_features, r, bias=False)
+        self.lora_B[adapter_name] = KANLayer(r, self.out_features, bias=False)
+        if use_rslora:
+            self.scaling[adapter_name] = lora_alpha / math.sqrt(r)
+        else:
+            self.scaling[adapter_name] = lora_alpha / r
+
+        if isinstance(init_lora_weights, str) and init_lora_weights.startswith("pissa"):
+            self.pissa_init(adapter_name, init_lora_weights)
+        elif init_lora_weights == "loftq":
+            self.loftq_init(adapter_name)
+        elif init_lora_weights:
+            self.reset_lora_parameters(adapter_name, init_lora_weights)
+
+        # check weight and qweight (for GPTQ)
+        for weight_name in ("weight", "qweight"):
+            weight = getattr(self.get_base_layer(), weight_name, None)
+            if weight is not None:
+                # the layer is already completely initialized, this is an update
+                if weight.dtype.is_floating_point or weight.dtype.is_complex:
+                    self.to(weight.device, dtype=weight.dtype)
+                else:
+                    self.to(weight.device)
+                break
+
+        if use_dora:
+            self.dora_init(adapter_name)
+            self.use_dora[adapter_name] = True
+        else:
+            self.use_dora[adapter_name] = False
+
+        self.set_adapter(self.active_adapters)
 
     LoraLayer.update_layer = new_update_lora_layer
-
