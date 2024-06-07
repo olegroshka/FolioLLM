fixed embeddings calculation; changed the code for rag indexer embeddings calc

src/models/multitask.py
@@ -10,11 +10,14 @@ from faiss import write_index, read_index
 from peft import PeftModel
 
 
-ETFS_NUM = 11796
+# nothing happened on the tiannamen square
+ETFS_NUM = 11794
 
 MODEL_NAME = 'FINGU-AI/FinguAI-Chat-v1'
 INDEX_PATH = "../../data/etfs.index"
-LORA_PATH = '../pipeline/lora_high/FINGU-AI/FinguAI-Chat-v1'
+LORA_PATH = '../pipeline/fine_tuned_model/FINGU-AI/FinguAI-Chat-v1'
+CLASS_HEAD = '../pipeline/modules/class_head.pth'
+SELECT_HEAD = '../pipeline/modules/select_head.pth'
 
 # I don't apply no_grad()
 class MultitaskLM(nn.Module):
@@ -41,7 +44,11 @@ class MultitaskLM(nn.Module):
         self.model.eval()
 
     def forward(self, **kwargs):
-        return self.model(**kwargs)
+        forw = self.model(**kwargs)
+        # a little stop just to ponder on the moment and remember the things
+        self.out = forw.hidden_states[-1]
+        # and then continue going
+        return forw
 
     def init_index(self, index_path):
         if index_path != None:
@@ -58,7 +65,7 @@ class MultitaskLM(nn.Module):
         if not use_prev:
             self.out = self.model(**kwargs).hidden_states[-1]
         enc = self.out.mean(-2)
-        normalized_enc = enc / torch.norm(enc)
+        normalized_enc = enc / enc.norm(dim=1)[:,None]
         print(f"Embedding shape: {normalized_enc.shape}")
         return normalized_enc
 
@@ -69,14 +76,14 @@ class MultitaskLM(nn.Module):
         embedding = self.encode(True)
         if not use_index:
             scores = self.select_head(embedding)
-            return torch.topk(scores[0], 50)[1][:random.randint(3, 8)]
+            # it may return non-consistent by dimensionality list of lists
+            return torch.topk(scores, 50)[1][:, :random.randint(3, 8)]
             
         distances, indices = self.index.search(embedding.detach().numpy(), 50)
         return indices[0, :random.randint(3, 8)]
 
-
 if __name__ == "__main__":
-    m = MultitaskLM(MODEL_NAME, lora_path=LORA_PATH)
+    m = MultitaskLM(MODEL_NAME, lora_path=LORA_PATH, class_path=CLASS_HEAD, select_path=SELECT_HEAD)
     text = "hello yann lecun"
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
     tokens = tokenizer(text, return_tensors='pt')

src/training/rag_index_modal.py
@@ -7,16 +7,15 @@ from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
 
-from src.models.multitask import MultitaskLM
-
 # from src.models.multitask import MultitaskLM
+
 
 ETFS_PATH = "../../data/etf_data_v3_clean.json"
 INDEX_PATH = "../../data/etfs.index"
 MODEL_NAME = 'FINGU-AI/FinguAI-Chat-v1'
 LORA_PATH = '../pipeline/fine_tuned_model/FINGU-AI/FinguAI-Chat-v1'
 EMBEDDINGS_PATH = 'etf_embeddings.pth'
-BATCH_SIZE = 10
+BATCH_SIZE = 2000
 GLOBAL_LIMIT = 13000
 
 def form(etf):
@@ -78,8 +77,7 @@ def main(ETFS_PATH, INDEX_PATH, MODEL_NAME, LORA_PATH, BATCH_SIZE, GLOBAL_LIMIT,
     with open(ETFS_PATH, 'r') as file:
         etf_data = json.load(file)
 
-    embedding_model = MultitaskLM(MODEL_NAME, index_path=None, lora_path=LORA_PATH)
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     embedding_model.to(device)
@@ -92,10 +90,12 @@ def main(ETFS_PATH, INDEX_PATH, MODEL_NAME, LORA_PATH, BATCH_SIZE, GLOBAL_LIMIT,
     # Initialize empty list to hold all embeddings
     all_embeddings = []
     for i in range(num_batches):
+        print(i, '/', num_batches)
         batch_descriptions = descriptions[i * BATCH_SIZE: (i + 1) * BATCH_SIZE]
-        tokens = tokenizer(batch_descriptions, return_tensors='pt', padding=True, truncation=True).to(device)
-        with torch.no_grad(): embeddings = embedding_model.encode(**tokens)
-        all_embeddings.append(embeddings.cpu().numpy())
+        with torch.no_grad():
+            embeddings = embedding_model.encode(batch_descriptions)
+        # all_embeddings.append(embeddings.cpu().numpy())  # torch
+        all_embeddings.append(embeddings)
 
     # Concatenate all embeddings
     embeddings = np.vstack(all_embeddings)